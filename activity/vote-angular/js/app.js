3145d2d33d8a962ab41cebe60df92c442479937c
Tony <lostyu789@163.com>
2016-06-21 19:12:18 +0800

    angular-vote3

--- activity/vote-angular/js/app.js
+++ activity/vote-angular/js/app.js
@@ -19,27 +19,42 @@ appModule.run(['$rootScope', '$state', '$stateParams', '$location',
 // config
 appModule.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/index');
+    // $urlRouterProvider.otherwise('/index');
+
+    // $stateProvider
+    // 	.state("index", {
+    //         url: "/index",
+    //         templateUrl: "tpls/index.html"
+    //     })
+    //     .state("list", {
+    //         url: "/list",
+    //         templateUrl: "tpls/list.html",
+    //         controller: 'listCtrl'
+    //     })
+    //     .state("detail", {
+    //         url: "/detail/:id",
+    //         templateUrl: "tpls/detail.html",
+    //         controller: 'detailCtrl'
+    //     })
+    //     .state("signUp", {
+    //         url: "/signUp",
+    //         templateUrl: "tpls/signUp.html",
+    //         controller: 'signUpCtrl'
+    //     })
 
     $stateProvider
-    	.state("index", {
-            url: "/index",
-            templateUrl: "tpls/index.html"
-        })
         .state("list", {
-            url: "/list",
-            templateUrl: "tpls/list.html",
-            controller: 'listCtrl'
-        })
-        .state("detail", {
-            url: "/detail/:id",
-            templateUrl: "tpls/detail.html",
-            controller: 'detailCtrl'
-        })
-        .state("signUp", {
-            url: "/signUp",
-            templateUrl: "tpls/signUp.html",
-            controller: 'signUpCtrl'
+            url: '/list',
+            views: {
+                'list': {
+                    templateUrl: "tpls/list.html",
+                    controller: 'listCtrl'
+                },
+                'detail': {
+                    templateUrl: "tpls/detail.html",
+                    controller: 'detailCtrl'
+                }
+            }
         })
 
 });
@@ -94,15 +109,104 @@ appModule.controller('detailCtrl', function($scope, $http, $stateParams){
     });
 
 });
-appModule.controller('signUpCtrl', function($scope, $http){
+appModule.controller('signUpCtrl', function($scope){
 
-    $scope.checkForm = function(){
+    $scope.showNext = false;
+
+    $scope.user = {};
+    $scope.user.text = '';
+    $scope.len = 0;
+
+    $scope.reg =/^\d{11}$/;
+    $scope.isUnTel = true;
+
+    $scope.imgUrl = 'xxx';
+    $scope.showImgBox = false;
+
+    $scope.checkValid = function(formValid){
+        console.log($scope.myForm.utel.$invalid);
+        if(!formValid.$invalid && $scope.isUnTel == false){
+            $scope.showNext = true;
+            console.log($scope);
+        }
+    };
+
+    $scope.checkText = function(){
+
+        if($scope.user.text && $scope.user.text.length){
+            $scope.len = $scope.user.text.length;
+
+            if($scope.user.text.length > 200){
+                $scope.user.text = $scope.user.text.substr(0, 200);
+            }
+        }else{
+            $scope.len = 0;
+        }
 
     };
+
+    $scope.checkTel = function(){
+        if(!$scope.reg.test($scope.user.tel)){
+            $scope.isUnTel = true;
+        }else{
+            $scope.isUnTel = false;
+        }
+    };
+
+    $scope.uploadFile = function(event){
+
+        // 如果浏览器不支持FileReader，则不处理  
+  
+        if (!window.FileReader) return;  
+      
+        var files = event.target.files;  
+      
+        for (var i = 0, f; f = files[i]; i++) {  
+      
+            if (!f.type.match('image.*')) {  
+      
+                continue;  
+      
+            }  
+      
+      
+            var reader = new FileReader();  
+      
+            reader.onload = (function(theFile) {  
+      
+                return function(e) {  
+      
+                    
+      
+                    $scope.imgUrl = e.target.result;  
+                    $scope.$apply();
+      
+                };  
+      
+            })(f);  
+      
+      
+            reader.readAsDataURL(f);  
+      
+        }  
+        $scope.showImgBox = true;
+        
+    };
+
 
 });
 
 // directive
+appModule.directive('customOnChange', function() {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var onChangeHandler = scope.$eval(attrs.customOnChange);
+      element.bind('change', onChangeHandler);
+    }
+  };
+});
+
 //appModule.directive('modal', function(){
 //    return{
 //        restrict: 'E',
