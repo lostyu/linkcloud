787f3c4876fff17c362937b56ca1292fe94f838f
Tony <lostyu789@163.com>
2016-06-29 16:45:04 +0800

    weiSite init

---
+++ weiSite/js/app.js
@@ -0,0 +1,27 @@
+$(function () {
+    ws.ui.swiper();
+    ws.ui.preloader();
+});
+
+window.ws = {};
+
+ws.ui = {};
+ws.ui.swiper = function () {
+    var mySwiper = new Swiper ('.swiper-container', {
+        //loop: true,
+        // 如果需要分页器
+        pagination: '.swiper-pagination'
+
+    })
+};
+ws.ui.preloader = function(){
+    var loading = false;  //状态标记
+    $('.j-loader').infinite().on("infinite", function() {
+        if(loading) return;
+        loading = true;
+        setTimeout(function() {
+            $(".j-listCnt").append("<p> 我是新加载的内容 </p>");
+            loading = false;
+        }, 1500);   //模拟延迟
+    });
+};
